e6d9e88054be54d883ec875a289cc4f7ceabbc61
Sarvesh Singh <40350507+sarvesh371@users.noreply.github.com>
2020-11-30 09:50:02 +0530

    Update venv_setup.sh

--- venv_setup.sh
+++ venv_setup.sh
@@ -1,7 +1,7 @@
 export PYTHONHTTPSVERIFY=0
 export PYTHON_VENV=venv
 rm -rf $PYTHON_VENV
-pip install virtualenv
+python3 -m pip install virtualenv
 python3 -m virtualenv $PYTHON_VENV
 source $PYTHON_VENV/bin/activate
 python3 -m pip install -U pip
