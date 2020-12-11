6963020689b67835437bf48619df36f18a39267e
sarvesh.singh <sarvesh.singh@cyware.com>
2020-12-11 19:07:54 +0530

    windows update

--- venv_setup_windows.bat
+++ venv_setup_windows.bat
@@ -1,9 +1,9 @@
-export PYTHONHTTPSVERIFY=0
-export PYTHON_VENV=venv
-rm -rf $PYTHON_VENV
+set PYTHONHTTPSVERIFY=0
+del /f /s /q venv 1>nul
+rmdir /s /q venv
 python -m pip install virtualenv
-python -m virtualenv $PYTHON_VENV
-source $PYTHON_VENV/Scripts/activate
+python -m virtualenv venv
+venv\Scripts\activate
 python -m pip install -U pip
 python -m pip install -U virtualenv
 python -m pip install --progress-bar pretty --requirement requirements.txt
